ec30d69a5fe9f701afdfc6d26de93ff834a644ea
keshavsoft <nknnkumar@yahoo.com>
2026-06-26 22:11:29 +0530

    test v1

---
+++ test/v1/TableComp/test.js
@@ -0,0 +1,15 @@
+import { showAll } from "../../../index.js";
+import path from "path";
+
+const folderName = process.argv[2];
+const tableName = process.argv[3];
+const showLog = process.argv[4];
+
+// const run = await load();
+
+await showAll({
+    toPath: path.join(process.cwd(), folderName),
+    tableName,
+    configPath: path.join(process.cwd(), "Config", "Schemas"),
+    showLog
+});
