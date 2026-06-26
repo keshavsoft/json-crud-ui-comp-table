55b9b5ae8c3d2610a153537b8bf7634cdabe7dbd
keshavsoft <nknnkumar@yahoo.com>
2026-06-26 22:00:21 +0530

    v7 start

---
+++ bin/v7/commands/TableComp/steps/AlterJson/index.js
@@ -0,0 +1,47 @@
+import fs from "fs";
+import path from "path";
+
+import updateColumnsConfig from "./updateColumnsConfig.js";
+
+const readJson = (filePath) => {
+    return JSON.parse(fs.readFileSync(filePath, "utf8"));
+};
+
+const writeJson = (filePath, data) => {
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 4));
+};
+
+const getSchemaConfig = ({ fromConfigPath, tableName, showLog }) => {
+    const schemaPath = path.join(
+        fromConfigPath,
+        `${tableName}.json`
+    );
+
+    const schemaPathAsJson = readJson(schemaPath);
+
+    if (showLog) if (!"columnsConfig" in schemaPathAsJson) console.log("columnsConfig not found in schema source");
+
+    return schemaPathAsJson?.columnsConfig;
+};
+
+const getShowAllConfigPath = ({ inDestination }) => {
+    return path.join(
+        inDestination,
+        "Index",
+        "Configs"
+    );
+};
+
+const startFunc = ({ inDestination, tableName, fromConfigPath, showLog = false }) => {
+    const columnsConfig = getSchemaConfig({ fromConfigPath, tableName, showLog });
+
+    const configRoot = getShowAllConfigPath({ inDestination });
+
+    updateColumnsConfig({
+        configRoot,
+        columnsConfig,
+        tableName
+    });
+};
+
+export default startFunc;
---
+++ bin/v7/start.js
@@ -0,0 +1,33 @@
+import parseInput from "./core/parseInput.js";
+import showUsage from './core/showUsage.js';
+
+import resolveCommand from './core/resolveCommand.js';
+
+import tableComp from "./commands/tableComp.js";
+
+import pkg from '../../package.json' with { type: 'json' };
+
+const version = pkg.version;
+
+const run = async ({ type, tableName, showLog, toPath, configPath }) => {
+  if (!configPath) {
+    console.log("configPath not supplied ");
+  };
+
+  const input = parseInput({
+    inTableName: tableName,
+    inShowLog: showLog, inToPath: toPath,
+    configPath
+  });
+
+  if (input.action === "--help" || input.action === "-h" || input.action === "help") return showUsage(version);
+
+  await tableComp({
+    tableName: input.tableName,
+    showLog: input.showLog,
+    toPath: input.toPath,
+    configPath: input.configPath
+  });
+};
+
+export default run;
